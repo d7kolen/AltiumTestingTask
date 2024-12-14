a7632845639cb9289487e7880aefd23cba040e4f
dsemikolenov-stenn <dmitri.semikolenov@stenn.com>
2024-12-14 18:31:32 +0300

    Initial

---
+++ Altium.Core/Row/RowDtoSorting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Altium.Core;
+
+public class RowDtoSorting
+{
+    public RowDto Data { get; }
+    public RowDtoSorting? Next { get; set; }
+
+    public RowDtoSorting(RowDto data, RowDtoSorting? next = null)
+    {
+        Data = data;
+        Next = next;
+    }
+}
---
+++ Altium.Tests/DynamicSortTests.cs
@@ -0,0 +1,33 @@
+﻿using Altium.Core;
+using FluentAssertions;
+using NUnit.Framework;
+using Serilog;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altium.Tests;
+
+[TestFixture]
+public class DynamicSortTests
+{
+    #region Init
+
+    RowDtoAlphabet _alphabet;
+
+    [SetUp]
+    public void Init()
+    {
+        _alphabet = new RowDtoAlphabet();
+    }
+
+    #endregion
+
+    [Test]
+    public async Task ReadRow()
+    {
+        var r0 = new RowDtoSorting(new RowDto("1. abc", _alphabet));
+        var r1 = new RowDtoSorting(new RowDto("1. abc", _alphabet), r0);
+        var root = new RowDtoSorting(new RowDto("1. abc", _alphabet), r1);
+
+    }
+}
